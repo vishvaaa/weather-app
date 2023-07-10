233f58a036dfe8165a62772d8523b05f5941416e
vishvaas M <139100530+vishvaaa@users.noreply.github.com>
2023-07-10 13:54:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React,{useState} from "react";
+
+const App = () => {
+  const [city,setCity] = useState("")
+  const [result,setResult] = useState("")
+  const changeHandler = e =>{
+    setCity(e.target.value);
+  }
+  const submitHandler = e =>{
+    e.preventDefault();
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then(
+      response => response.json()
+    ).then(data => {
+      const kelvin = data.main.temp;
+      const celsius = kelvin - 273.15
+      setResult("Temperature at"+" "+city+"\n"+Math.round(celsius)+"°C")
+      setCity("");
+    }).catch(error => console.log(error))
+  }
+  return (
+    <div className="App">
+      <center>
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title">Weather Application</h4>
+            <form onSubmit={submitHandler}>
+              <input type="text" name="city" value={city} onChange={changeHandler} /><br/><br/>
+              <input type="submit" value="Get Temperature" />
+            </form>
+            <h1>{result}</h1>
+          </div>
+        </div>
+      </center>
+    </div>
+  );
+}
+
+export default App;
